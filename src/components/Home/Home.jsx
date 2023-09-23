6dfdba9c848a63e8a42dd92cf024b555d6ea7f08
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-09-23 10:13:16 +0600

    using react awesome reval

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,76 +1,25 @@
-import React, { useEffect, useRef } from "react";
 import About from "../About/About";
 import WhatIDo from "../WhatIDo/WhatIDo";
 import MySkills from "../Skills/MySkills";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import ContactMe from "../Contact/ContactMe";
-
-gsap.registerPlugin(ScrollTrigger);
+import { Fade, Slide, Zoom } from "react-awesome-reveal";
 
 const Home = () => {
-  const aboutRef = useRef(null);
-  const whatIDoRef = useRef(null);
-  const skillsRef = useRef(null);
-  const contactRef = useRef(null);
-
-  useEffect(() => {
-    // Define a common animation configuration
-    const commonAnimation = {
-      duration: 1500, // Adjust the duration as needed
-      opacity: 100,
-      y: 80, // Adjust the initial position as needed
-      ease: "power3.out", // Adjust the easing function as needed
-    };
-
-    // Create GSAP animations for each section
-    gsap.from(aboutRef.current, {
-      ...commonAnimation,
-      scrollTrigger: {
-        trigger: aboutRef.current,
-        toggleActions: "restart none reverse none",
-      },
-    });
-
-    gsap.from(whatIDoRef.current, {
-      ...commonAnimation,
-      scrollTrigger: {
-        trigger: whatIDoRef.current,
-        toggleActions: "restart none reverse none",
-      },
-    });
-
-    gsap.from(skillsRef.current, {
-      ...commonAnimation,
-      scrollTrigger: {
-        trigger: skillsRef.current,
-        toggleActions: "restart none reverse none",
-      },
-    });
-
-    gsap.from(contactRef.current, {
-      ...commonAnimation,
-      scrollTrigger: {
-        trigger: contactRef.current,
-        toggleActions: "restart none reverse none",
-      },
-    });
-  }, []);
 
   return (
     <div className="px-8 bg-slate-600hu">
-      <div className="my-4" ref={aboutRef}>
+      <Slide className="my-4">
         <About />
-      </div>
-      <div className="my-4" ref={whatIDoRef}>
+      </Slide>
+      <Zoom className="my-4">
         <WhatIDo />
-      </div>
-      <div className="my-4" ref={skillsRef}>
+      </Zoom>
+      <Slide className="my-4">
         <MySkills />
-      </div>
-      <div className="my-4" ref={contactRef}>
+      </Slide>
+      <Zoom className="my-4">
         <ContactMe />
-      </div>
+      </Zoom>
     </div>
   );
 };
